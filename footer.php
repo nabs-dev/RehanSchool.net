ee96afe51ec63bd2c747423654bbdfe2a63b48a4
nabs-dev <nabihakashif631@gmail.com>
2025-07-29 20:05:02 +0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <footer>
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> Rehan School. All Rights Reserved.</p>
+        </div>
+    </footer>
+
+    <script>
+        // Page Redirection Function
+        function redirectTo(page) {
+            window.location.href = page;
+        }
+    </script>
+</body>
+</html>
